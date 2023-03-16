468518f311d68d247741e0b97f3008a259a28034
JusticHentai <1360636180@qq.com>
2023-03-16 10:31:04 +0800

    fix: test

--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
 #! /usr/bin/env node
 
-import configParsing from './test/config-parsing'
-configParsing()
+const a = (n: number) => {
+  const args = Array.prototype.slice.call(arguments)
+  console.log(args)
+  console.log('分割线')
+  console.log(...arguments)
+}
+
+a(5)
