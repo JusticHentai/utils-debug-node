2c107b4636ad00fb7d711bd14060e424f190109c
JusticHentai <1360636180@qq.com>
2023-03-18 22:08:49 +0800

    fix: del code

--- index.ts
+++ index.ts
@@ -1,10 +1,1 @@
 #! /usr/bin/env node
-
-const a = (n: number) => {
-  const args = Array.prototype.slice.call(arguments)
-  console.log(args)
-  console.log('分割线')
-  console.log(...arguments)
-}
-
-a(5)
